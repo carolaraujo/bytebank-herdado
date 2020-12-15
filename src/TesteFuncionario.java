e83efb5ad4f5fe61b925d8155d12cf4908ff5765
Caroline Araujo <ca.araujosouza@gmail.com>
2020-12-15 18:38:31 -0300

    first commit

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,17 @@
+
+public class TesteFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("Nico Steppat");
+		nico.setCpf("2226637-89");
+		nico.setSalario(2600.0);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+				
+	}
+
+}
