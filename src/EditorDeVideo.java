93ae8d99e58794fe03079f3139cdfd251653e26a
Caroline Araujo <ca.araujosouza@gmail.com>
2020-12-15 19:25:49 -0300

    polimorfismos em funcionarios

---
+++ src/EditorDeVideo.java
@@ -0,0 +1,9 @@
+
+public class EditorDeVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Editor de video");
+		return super.getBonificacao() + 100;
+	}
+
+}
